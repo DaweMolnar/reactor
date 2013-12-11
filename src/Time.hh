22128a2d2d1372b3b62b04d682befe01979b2906
László ÁSHIN <laszlo@ashin.hu>
2013-12-11 21:57:02 +0100

    detach Time.hh

---
+++ src/Time.hh
@@ -0,0 +1,32 @@
+#ifndef REACTOR_TIME_HEADER
+#define REACTOR_TIME_HEADER
+
+#include "DiffTime.hh"
+
+class Time {
+	uint64_t time_;
+
+	explicit Time(uint64_t time) : time_(time) {}
+
+public:
+	static Time now();
+
+	Time() : time_(0) {}
+
+	DiffTime operator-(const Time &rhs) const { return DiffTime::raw(time_ - rhs.time_); }
+
+	Time operator+(const DiffTime &rhs) const { return Time(time_ + rhs.raw()); }
+	Time &operator+=(const DiffTime &rhs) { time_ += rhs.raw(); return *this; }
+	Time operator-(const DiffTime &rhs) const { return Time(time_ - rhs.raw()); }
+	Time &operator-=(const DiffTime &rhs) { time_ -= rhs.raw(); return *this; }
+
+	bool operator<(const Time &rhs) const { return time_ < rhs.time_; }
+
+	time_t unixtime() const { return time_ >> 32; }
+	uint32_t fraction(uint32_t multiplier) const { return ((time_ & 0xffffffff) * multiplier) >> 32; }
+	uint32_t msFraction() const { return fraction(1000); }
+	uint32_t usFraction() const { return fraction(1000 * 1000); }
+	uint32_t nsFraction() const { return fraction(1000 * 1000 * 1000); }
+};
+
+#endif // REACTOR_TIME_HEADER
--- src/client.cc
+++ src/client.cc
@@ -1,7 +1,7 @@
 #include "ErrnoException.hh"
 #include "StreamSock.hh"
 #include "ActionMethod.hh"
-#include "DiffTime.hh"
+#include "Time.hh"
 
 #include <stdint.h>
 #include <cstring>
@@ -20,32 +20,6 @@
 #include <memory> // auto_ptr
 
 #define ARRAY_LENGTH(x) (sizeof(x) / sizeof(x[0]))
-
-class Time {
-	uint64_t time_;
-
-	explicit Time(uint64_t time) : time_(time) {}
-
-public:
-	static Time now();
-
-	Time() : time_(0) {}
-
-	DiffTime operator-(const Time &rhs) const { return DiffTime::raw(time_ - rhs.time_); }
-
-	Time operator+(const DiffTime &rhs) const { return Time(time_ + rhs.raw()); }
-	Time &operator+=(const DiffTime &rhs) { time_ += rhs.raw(); return *this; }
-	Time operator-(const DiffTime &rhs) const { return Time(time_ - rhs.raw()); }
-	Time &operator-=(const DiffTime &rhs) { time_ -= rhs.raw(); return *this; }
-
-	bool operator<(const Time &rhs) const { return time_ < rhs.time_; }
-
-	time_t unixtime() const { return time_ >> 32; }
-	uint32_t fraction(uint32_t multiplier) const { return ((time_ & 0xffffffff) * multiplier) >> 32; }
-	uint32_t msFraction() const { return fraction(1000); }
-	uint32_t usFraction() const { return fraction(1000 * 1000); }
-	uint32_t nsFraction() const { return fraction(1000 * 1000 * 1000); }
-};
 
 Time
 Time::now()
