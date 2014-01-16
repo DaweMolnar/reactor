e919bc2afcc08d037521b818c9e9927fbeeac349
László ÁSHIN <laszlo@ashin.hu>
2014-01-16 20:55:10 +0100

    testUnits: use CompilerOutputter

--- tests/unit/testUnits.cc
+++ tests/unit/testUnits.cc
@@ -1,5 +1,6 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/CompilerOutputter.h>
 
 #include <cstdlib>
 
@@ -9,7 +10,11 @@ main()
 	CppUnit::TextUi::TestRunner runner;
 	CppUnit::TestFactoryRegistry &registry =
 		CppUnit::TestFactoryRegistry::getRegistry();
+	CppUnit::CompilerOutputter *outputter =
+		new CppUnit::CompilerOutputter(&runner.result(), std::cerr);
 
 	runner.addTest(registry.makeTest());
+	outputter->setLocationFormat("%p:%l:");
+	runner.setOutputter(outputter);
 	return runner.run("", false) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
