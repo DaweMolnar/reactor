4a7491d4d9e5b2ca8f765506ae55bfc6ce8252a8
László ÁSHIN <laszlo@ashin.hu>
2014-01-19 21:45:28 +0100

    ClientTester: check netcat variant

--- tests/func/script/ClientTester.sh
+++ tests/func/script/ClientTester.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-nc -lp 9243 > out/a &
+if nc -V | grep 'GNU Netcat' >/dev/null 2>&1; then
+	NCOPTS=-lp
+else
+	NCOPTS=-l
+fi
+
+nc "${NCOPTS}" 9243 > out/a &
 echo valami > out/b
 sleep 0.1
 ( cat out/b; sleep 0.1 ) | out/testFuncs ClientTester 127.0.0.1 9243 2>/dev/null
